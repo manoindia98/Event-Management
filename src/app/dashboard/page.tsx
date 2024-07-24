312c3645bb4e6636cbad50ed20ffdf206062a7b6
X-itachi-X <ankitprakash2909@gmail.com>
2024-07-24 13:24:12 +0530

    Defining Homepage in dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,77 @@
 import React from 'react'
+import { Button } from "../../components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "../../components/ui/card"
+import { Input } from "../../components/ui/input"
+import { Label } from "../../components/ui/label"
+
 
 function page() {
   return (
-    <>
-      <div>Ads/Promo running</div>
-      <div>Upcoming Bookings For Event</div>
-      <div>Know our top Artists</div>
+    <>  
+      
+      <div className='my-8 mx-4'>
+      <Card className="w-full">
+        <CardHeader>
+          <CardTitle>Ads/Promo running</CardTitle>
+          <CardDescription>Your Packags currently Active</CardDescription>
+        </CardHeader>
+        <CardContent>
+          
+        </CardContent>
+        <CardFooter className="flex justify-between">
+          
+        </CardFooter>
+      </Card>
+      </div>
+      <div className='my-8 mx-4'>
+      <Card className="w-full">
+        <CardHeader>
+          <CardTitle>Upcoming Bookings For Event</CardTitle>
+          <CardDescription>Your Upcomming Bookings</CardDescription>
+        </CardHeader>
+        <CardContent>
+          
+        </CardContent>
+        <CardFooter className="flex justify-between">
+          
+        </CardFooter>
+      </Card>
+      </div>
+      <div className='my-8 mx-4'>
+      <Card className="w-full">
+        <CardHeader>
+          <CardTitle>Top trending deals</CardTitle>
+          <CardDescription>Grab these deals before it runs out</CardDescription>
+        </CardHeader>
+        <CardContent>
+          
+        </CardContent>
+        <CardFooter className="flex justify-between">
+          
+        </CardFooter>
+      </Card>
+      </div>
+      <div className='my-8 mx-4'>
+      <Card className="w-full">
+        <CardHeader>
+          <CardTitle>Know our top Artists</CardTitle>
+          <CardDescription>Top trending artists and their works</CardDescription>
+        </CardHeader>
+        <CardContent>
+          
+        </CardContent>
+        <CardFooter className="flex justify-between">
+          
+        </CardFooter>
+      </Card>
+      </div>
     </>
   )
 }
