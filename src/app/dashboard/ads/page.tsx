2c86c806a157e1b3ff58288321736a7f0e5ddab8
X-itachi-X <ankitprakash2909@gmail.com>
2024-07-27 16:21:47 +0530

    added desktop layout + bestseller component

--- src/app/dashboard/ads/page.tsx
+++ src/app/dashboard/ads/page.tsx
@@ -3,7 +3,6 @@ import React from 'react'
 function page() {
   return (
     <>
-      <div>Search Packages</div>
       <div>Active Packages</div>
       <div>Stats for Package running and reaches</div>
       <div>Package list available to sell</div>
