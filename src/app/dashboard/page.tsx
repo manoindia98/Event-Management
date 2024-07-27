8365f059c63e34ec8bc786e25ad437cdc60b607d
X-itachi-X <ankitprakash2909@gmail.com>
2024-07-27 17:08:25 +0530

    resolved responsive bug

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -18,60 +18,11 @@ function page() {
     <>  
       
       <div className='my-8 mx-4'>
-      <Card className="w-full">
-        <CardHeader>
-          <CardTitle>Ads/Promo running</CardTitle>
-          <CardDescription>Your Packags currently Active</CardDescription>
-        </CardHeader>
-        <CardContent>
-          
-        </CardContent>
-        <CardFooter className="flex justify-between">
-          
-        </CardFooter>
-      </Card>
-      </div>
-      <div className='my-8 mx-4'>
-      <Card className="w-full">
-        <CardHeader>
-          <CardTitle>Upcoming Bookings For Event</CardTitle>
-          <CardDescription>Your Upcomming Bookings</CardDescription>
-        </CardHeader>
-        <CardContent>
-          
-        </CardContent>
-        <CardFooter className="flex justify-between">
-          
-        </CardFooter>
-      </Card>
-      </div>
-      <div className='my-8 mx-4'>
-      <Card className="w-full">
-        <CardHeader>
-          <CardTitle>Top trending deals</CardTitle>
-          <CardDescription>Grab these deals before it runs out</CardDescription>
-        </CardHeader>
-        <CardContent>
-          
-        </CardContent>
-        <CardFooter className="flex justify-between">
-          
-        </CardFooter>
-      </Card>
-      </div>
-      <div className='my-8 mx-4'>
-      <Card className="w-full">
-        {/* <CardHeader>
-          <CardTitle>Know our top Artists</CardTitle>
-          <CardDescription>Top trending artists and their works</CardDescription>
-        </CardHeader> */}
-        <CardContent>
-          <AppleCardsCarouselDemo />
-        </CardContent>
-        <CardFooter className="flex justify-between">
-          
-        </CardFooter>
-      </Card>
+        <div>Ads/Promo running</div>
+        <div>Upcoming Bookings For Event</div>
+        <div><AppleCardsCarouselDemo /></div>
+        <div></div>
+        <div></div>
       </div>
     </>
   )
