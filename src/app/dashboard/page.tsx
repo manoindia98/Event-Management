d34bc72b1ad8d9837f6ef64cfd7ec81d8d7a1f3e
X-itachi-X <ankitprakash2909@gmail.com>
2024-07-27 20:03:34 +0530

    worked on Dashboard Home

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -11,6 +11,8 @@ import {
 import { Input } from "../../components/ui/input"
 import { Label } from "../../components/ui/label"
 import { AppleCardsCarouselDemo } from '@/components/AppleCardsCarouselDemo'
+import { WobbleCardDemo } from '@/components/WobbleCardDemo'
+import { WobbleCard } from '@/components/ui/wobble-card'
 
 
 function page() {
@@ -18,8 +20,52 @@ function page() {
     <>  
       
       <div className='my-8 mx-4'>
-        <div>Ads/Promo running</div>
-        <div>Upcoming Bookings For Event</div>
+        <div className='my-10'>
+          <h2 className="max-w-7xl pl-4 mx-auto text-2xl md:text-5xl font-bold text-neutral-800 dark:text-neutral-200">
+        Ads/Promo running
+      </h2>
+      {/* <Card className='w-[12rem] p-10 m-8'>
+        <CardContent>
+          asa
+        </CardContent>
+      </Card> */}
+      <div className='p-10 w-[40rem]'>
+      <WobbleCard
+        containerClassName="col-span-1 lg:col-span-2 h-full bg-blue-800 h-[300px]"
+        className=""
+      >
+        <div className="max-w-xs">
+          <h2 className="text-left text-balance text-2xl lg:text-3xl font-semibold tracking-[-0.015em] text-white">
+            Running Package
+          </h2>
+          <p className="mt-4 text-left  text-base/6 text-neutral-200">
+            With over 100,000 mothly active bot users, Gippity AI is the most
+            popular AI platform for developers.
+          </p>
+        </div>
+      </WobbleCard>
+      </div>
+      
+      </div>
+        <div className='my-10'><h2 className="max-w-7xl pl-4 mx-auto text-2xl md:text-5xl font-bold text-neutral-800 dark:text-neutral-200">
+        Upcoming Bookings For Event
+      </h2>
+      <div className='p-10 w-[40rem]'>
+      <WobbleCard
+        containerClassName="col-span-1 lg:col-span-2 h-full bg-purple-900 h-[300px]"
+        className=""
+      >
+        <div className="max-w-xs">
+          <h2 className="text-left text-balance text-2xl lg:text-3xl font-semibold tracking-[-0.015em] text-white">
+            Your Upcoming Booking For Artists
+          </h2>
+          <p className="mt-4 text-left  text-base/6 text-neutral-200">
+            With over 100,000 mothly active bot users, Gippity AI is the most
+            popular AI platform for developers.
+          </p>
+        </div>
+      </WobbleCard>
+      </div></div>
         <div><AppleCardsCarouselDemo /></div>
         <div></div>
         <div></div>
